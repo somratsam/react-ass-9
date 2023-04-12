6410d638e88555481aed68a10f074e74cab56929
somratsam <somratsam2@gmail.com>
2023-04-12 17:12:46 +0600

    fixed errror page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Link, useRouteError } from 'react-router-dom';
 import { FaceFrownIcon } from '@heroicons/react/24/solid'
+import { Button } from 'react-bootstrap';
 const ErrorPage = () => {
     const { error, status } = useRouteError()
     return (
         <div>
              <section className='container text-center'>
       <div className=''>
-        <FaceFrownIcon className=' text-warning' style={{ width: '32rem' }} />
+        <FaceFrownIcon className=' text-warning w-25' />
         <div className=''>
-          <h2 className=''>
-            <span className=''>Error</span>
+          <h2 className='text-warning' style={{ fontSize: '4rem' }}>
+            <span>Error</span>
             {status || 404}
           </h2>
           <h2 className='fw-bold text-danger'>
             {error?.message}
           </h2>
-          <Link to='/' className=' text-decoration-none'>
+         <Button variant='success mt-3'> <Link to='/' className=' text-white text-decoration-none'>
             Back to homepage
-          </Link>
+          </Link></Button>
         </div>
       </div>
     </section>
